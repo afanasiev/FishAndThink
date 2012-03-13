5b2804093e11bb685edf660456c23455bcb590dd
Sergey Afanasiev <afa@MacBook-Air-Serguei.local>
2012-03-13 17:59:36 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FishAndThink::Application.config.secret_token = '0400b58c8d6b66205c3758122c7faeaa49e71e60519753b393aeda5c4d5a95433aecb8420df1de5ebdc3bfb05818ca1d19a0ff90931ebc18d90131f9f8ffb197'
